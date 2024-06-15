5fb4a34a2b7769f8c068c19b4617b7a69fafcae5
crazerist <wuxizhoukangqi@gmail.com>
2024-06-15 15:04:17 +0800

    finish vfa_bagperm

--- vfa/BagPerm.v
+++ vfa/BagPerm.v
@@ -39,21 +39,32 @@ Definition bag_eqv (b1 b2: bag) : Prop :=
 
 Lemma bag_eqv_refl : forall b, bag_eqv b b.
 Proof.
-(* FILL IN HERE *) Admitted.
+  unfold bag_eqv; intros. reflexivity.
+Qed.
 
 Lemma bag_eqv_sym: forall b1 b2, bag_eqv b1 b2 -> bag_eqv b2 b1. 
 Proof.
-(* FILL IN HERE *) Admitted.
+  unfold bag_eqv; intros. rewrite H. reflexivity.
+Qed.
 
 Lemma bag_eqv_trans: forall b1 b2 b3, bag_eqv b1 b2 -> bag_eqv b2 b3 -> bag_eqv b1 b3.
 Proof.
-(* FILL IN HERE *) Admitted.
+  unfold bag_eqv; intros. rewrite H, H0. reflexivity.
+Qed.
 
 (** The following little lemma is handy in a couple of places. *)
 
 Lemma bag_eqv_cons : forall x b1 b2, bag_eqv b1 b2 -> bag_eqv (x::b1) (x::b2).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  unfold bag_eqv; intros. simpl. rewrite H. reflexivity.
+Qed.
+
+Lemma bag_eqv_cons_comm :
+  forall x1 x2 b,
+  bag_eqv (x1 :: x2 :: b) (x2 :: x1 :: b).
+Proof.
+  unfold bag_eqv; intros; simpl. lia.
+Qed.
 (** [] *)
 
 (* ################################################################# *)
@@ -75,14 +86,28 @@ Definition is_a_sorting_algorithm' (f: list nat -> list nat) :=
 
 Lemma insert_bag: forall x l, bag_eqv (x::l) (insert x l).
 Proof.
-(* FILL IN HERE *) Admitted.
+  induction l; intros; simpl.
+  - apply bag_eqv_refl.
+  - destruct (x <=? a) eqn : E.
+    + apply bag_eqv_refl.
+    + apply bag_eqv_trans with (a :: x :: l).
+      apply bag_eqv_cons_comm. apply bag_eqv_cons.
+      apply IHl.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, standard (sort_bag)
 
     Now prove that sort preserves bag contents. *)
-Theorem sort_bag: forall l, bag_eqv l (sort l).
-(* FILL IN HERE *) Admitted.
+Theorem sort_bag: 
+  forall l, bag_eqv l (sort l).
+Proof.
+  induction l; simpl.
+  - apply bag_eqv_refl.
+  - eapply bag_eqv_trans with (a :: (sort l)).
+    + apply bag_eqv_cons. apply IHl.
+    + apply insert_bag.
+Qed.
 (** [] *)
 
 (** Now we wrap it all up.  *)
@@ -128,7 +153,13 @@ Definition manual_grade_for_permutations_vs_multiset : option (nat*string) := No
 Lemma perm_bag:
   forall al bl : list nat,
    Permutation al bl -> bag_eqv al bl. 
-(* FILL IN HERE *) Admitted.
+Proof.
+  intros. induction H.
+  - apply bag_eqv_refl.
+  - apply bag_eqv_cons. apply IHPermutation.
+  - apply bag_eqv_cons_comm.
+  - eapply bag_eqv_trans. apply IHPermutation1. assumption.
+Qed.
 (** [] *)
 
 (** The other direction,
@@ -140,7 +171,10 @@ Lemma perm_bag:
 (** **** Exercise: 2 stars, advanced (bag_nil_inv) *)
 Lemma bag_nil_inv : forall b, bag_eqv [] b -> b = []. 
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. destruct b; try reflexivity.
+  unfold bag_eqv in H. specialize H with n; simpl in H.
+  rewrite Nat.eqb_refl in H. inversion H.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced (bag_cons_inv) *)
@@ -150,21 +184,68 @@ Lemma bag_cons_inv : forall l x n,
       l = l1 ++ x :: l2
       /\ count x (l1 ++ l2) = n.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction l; intros; simpl in *.
+  - inversion H.
+  - destruct (a =? x) eqn : E.
+    + apply Nat.eqb_eq in E; subst. injection H; intros.
+      exists [], l. simpl. split. reflexivity. 
+      symmetry. apply H0.
+    + simpl in H. apply IHl in H. destruct H as [l1 [l2 [H1 H2]]].
+      subst. exists (a :: l1), l2. split. reflexivity.
+      simpl. rewrite E. reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, advanced (count_insert_other) *)
 Lemma count_insert_other : forall l1 l2 x y,
     y <> x -> count y (l1 ++ x :: l2) = count y (l1 ++ l2).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction l1; intros; simpl.
+  - apply not_eq_sym in H. apply Nat.eqb_neq in H. 
+    rewrite H. reflexivity.
+  - f_equal. apply IHl1. apply H.
+Qed.
+
+Lemma count_insert_same :
+  forall x l1 l2,
+  count x (l1 ++ x :: l2) = S (count x (l1 ++ l2)).
+Proof.
+  induction l1; intros; simpl.
+  - rewrite Nat.eqb_refl. reflexivity.
+  - rewrite plus_n_Sm. f_equal. apply IHl1.
+Qed.
+
+Lemma count_app_comm :
+  forall n l1 l2,
+  count n (l1 ++ l2) = count n (l2 ++ l1).
+Proof.
+  induction l1; intros; simpl.
+  - rewrite app_nil_r. apply bag_eqv_refl.
+  - unfold bag_eqv; intros; simpl. destruct (a =? n) eqn : E.
+    + apply Nat.eqb_eq in E; subst. rewrite count_insert_same.
+      simpl. f_equal. apply IHl1.
+    + apply Nat.eqb_neq in E. apply not_eq_sym in E. 
+      rewrite (count_insert_other _ _ _ _ E).
+      rewrite Nat.add_0_l. apply IHl1.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced (bag_perm) *)
 Lemma bag_perm:
   forall al bl, bag_eqv al bl -> Permutation al bl.
 Proof.
-(* FILL IN HERE *) Admitted.
+  induction al; intros.
+  - apply bag_nil_inv in H; subst. constructor.
+  - unfold bag_eqv in H. specialize H with a as H0.
+    simpl in H0. rewrite Nat.eqb_refl in H0. simpl in H0.
+    apply bag_cons_inv in H0. destruct H0 as [l1 [l2 [H1 H2]]].
+    subst. 
+    assert (forall n, count n al = count n (l2 ++ l1)).
+    { intros. specialize H with n. rewrite count_app_comm in H.
+      simpl in H. apply Nat.add_cancel_l in H. assumption. }
+    apply (IHal (l2 ++ l1)) in H0. rewrite Permutation_app_comm.
+    simpl. constructor. apply H0.
+Qed.
 (** [] *)
 
 (* ################################################################# *)
@@ -187,6 +268,6 @@ Proof.
   apply bag_eqv_iff_perm; auto.
 Qed.
 
-(** $Date$ *)
+(** Date *)
 
-(* 2023-08-23 11:34 *)
+(* 2023-06-15 15:02 *)
